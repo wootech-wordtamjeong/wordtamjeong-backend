851bd1a05169567598a2164bd6329a64ea228b79
jhkim <juho1589@gmail.com>
2025-11-17 13:21:34 +0900

    feat(Util): add math.go
    - Math Utill Functional Collection

---
+++ pkg/utils/math.go
@@ -0,0 +1,24 @@
+package utils
+
+import "math"
+
+// CosineSimilarity calculates the cosine similarity between two vectors
+func CosineSimilarity(a, b []float64) float64 {
+	if len(a) != len(b) {
+		return 0.0
+	}
+
+	var dotProduct, normA, normB float64
+
+	for i := 0; i < len(a); i++ {
+		dotProduct += a[i] * b[i]
+		normA += a[i] * a[i]
+		normB += b[i] * b[i]
+	}
+
+	if normA == 0 || normB == 0 {
+		return 0.0
+	}
+
+	return dotProduct / (math.Sqrt(normA) * math.Sqrt(normB))
+}
