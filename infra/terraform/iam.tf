e4284f6e5f7a942649291e50fd91238e96f1dde4
kimjuho1559 <line1559@naver.com>
2026-09-04 17:51:21 +0900

    ci: deploy from develop branch

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -79,7 +79,7 @@ data "aws_iam_policy_document" "github_assume_role" {
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repository}:ref:refs/heads/master"]
+      values   = ["repo:${var.github_repository}:ref:refs/heads/develop"]
     }
   }
 }
